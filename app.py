538fbf18acc8aa62d7d5c36bf8d29e2e629eefc8
PushkarSharma12 <pushkarsharmabiz@gmail.com>
2020-08-10 20:10:48 +0530

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, url_for, request
+from flask import Flask,render_template, url_for, request, flash
 from flask import redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -7,37 +7,87 @@ from flask_wtf import FlaskForm
 from flask_wtf.csrf import CSRFProtect, CSRFError
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length 
+from models import *
+from wtforms_fields import *
+from passlib.hash import pbkdf2_sha256
+from flask_login import LoginManager, login_user, current_user, logout_user
+
+import os
+import time
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "thisisgood"
+app.secret_key='os.environ.get()'
+app.config['WTF_CSRF_SECRET_KEY'] = "b'f\xfa\x8b{X\x8b\x9eM\x83l\x19\xad\x84\x08\xaa"
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://pfflhnsoygfrwm:dbfb95b73b9e7f02027b474fd3255ab63ad6fe3c483194637e53b6ae0be90322@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d7km9rfhq4orfd'
+
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 Bootstrap(app)
+class RegistrationForm(FlaskForm):
+    """ Registration form"""
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min = 4, max = 20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min = 8 ,max = 80)])
+    username = StringField('username', validators=[InputRequired(message="Username required"), Length(min=4, max=25, message="Username must be between 4 and 25 characters")])
+    password = PasswordField('Password', validators=[InputRequired(message="Password required")])
+    email = StringField('Email', validators=[InputRequired(message="Email Required"),Email('Email should be valid!')])
 
-class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[InputRequired(), Length(min = 4, max = 20)])
-    username = StringField('Username', validators=[InputRequired(), Length(min = 4, max = 20)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min = 8 ,max = 80)])
-   
+    def validate_email(self, email):
+        user_object = User.query.filter_by(username=email.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists. Select a different username.")
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    
 
-@app.route('/login')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+
+    login_form = LoginForm()
+
+    # Allow login if validation success
+    if login_form.validate_on_submit():
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('dashboard'))
+
+    return render_template("login.html", form=login_form)
 
 
-    return render_template('login.html',form = form)
-
-
-@app.route('/signup')
+@app.route('/signup', methods=['GET','POST'])
 def signup():
-    form = RegisterForm()
+    form = RegistrationForm()  
+
+    if form.validate_on_submit():
+        username = form.username.data
+        Email = form.email.data
+        password = form.password.data
+        hashed_pwd = pbkdf2_sha256.hash(password)
+
+        email_object = User.query.filter_by(email=Email).first()
+        if email_object:
+            return 'Email already registered!'
+
+        user = User(username=username, email = Email, passworrd = hashed_pwd )
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect("/success")
+
     return render_template('signup.html',form = form)
 
 
@@ -45,6 +95,12 @@ def signup():
 def success():
     return render_template('success.html')
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
 
---
+++ wtforms_fields.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
+from passlib.hash import pbkdf2_sha256
+from models import User
+
+
+def invalid_credentials(form, field):
+    """ Username and password checker """
+
+    password = field.data
+    username = form.username.data
+
+    # Check username is invalid
+    user_data = User.query.filter_by(username=username).first()
+    if user_data is None:
+        raise ValidationError("Username or password is incorrect")
+
+    # Check password in invalid
+    elif not pbkdf2_sha256.verify(password, user_data.passworrd):
+        raise ValidationError("Username or password is incorrect")
+
+
+
+
+class LoginForm(FlaskForm):
+    """ Login form """
+
+    username = StringField('username', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password', validators=[InputRequired(message="Password required"), invalid_credentials])
